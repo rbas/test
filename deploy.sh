1a24eeb385f2f0d5cb825a2b7d4ab6c1894c4f62
Your Name <you@example.com>
2016-04-30 11:28:03 +0200

    blah

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ mkdir $BUILD_NAME
 mv * $BUILD_NAME
 tar -czf build.tgz $BUILD_NAME
 
-scp -P 11155 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
-ssh app@alpha-node-2.rosti.cz -p 11155 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
+scp -P 11155 -i id_dsa -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
+ssh app@alpha-node-2.rosti.cz -i id_dsa -p 11155 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
