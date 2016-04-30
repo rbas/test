2784215f1984212e181c6e7a66505f3463cb9783
Your Name <you@example.com>
2016-04-30 11:30:05 +0200

    blah

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ BUILD_NAME=build-$(date +"%Y-%m-%d-%y-%H:%M.%N")
 mkdir $BUILD_NAME
 mv * $BUILD_NAME
 tar -czf build.tgz $BUILD_NAME
-
+ls -alh
 scp -P 11155 -i id_dsa -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
 ssh app@alpha-node-2.rosti.cz -i id_dsa -p 11155 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
