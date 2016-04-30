32f90c7933b29d1a72cd573af23de64680ba1302
Your Name <you@example.com>
2016-04-30 11:18:25 +0200

    :snake:

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BUILD_NAME=build-$(date +"%Y-%m-%d-%y-%H:%M.%N")
+
+mkdir $BUILD_NAME
+mv * $BUILD_NAME
+tar -czf build.tgz $BUILD_NAME
+
+scp -P 11155 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
+ssh app@alpha-node-2.rosti.cz -p 11155 -o 'StrictHostKeyChecking no' "tar xfz  /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
